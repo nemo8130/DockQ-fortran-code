171f99c1690bb9d4897f1725f2cb09c3e68c72e4
nemo8130 <nemo8130@gmail.com>
2015-12-14 13:59:39 +0100

    4

---
+++ DockQ/clean.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm log.txt
+rm formch.out
+
+rm -f EXAMPLE_PDBS/*.scon
+rm -f EXAMPLE_PDBS/*.snet
+rm -f EXAMPLE_PDBS/*.spt
+rm -f EXAMPLE_PDBS/*.DockQ
+rm -f EXAMPLE_PDBS/*.renum
+rm -f EXAMPLE_PDBS/*.renum.fixed
+rm -f EXAMPLE_PDBS/*-unet.pdb
